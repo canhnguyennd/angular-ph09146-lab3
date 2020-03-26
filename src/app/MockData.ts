c56091d7ef61b8eb4b7fd9df230df7605940d84a
canhnguyennd <canhnguyennd0308@gmail.com>
2020-03-26 18:46:11 +0700

    3 files modified, 1 added

---
+++ src/app/MockData.ts
@@ -0,0 +1,25 @@
+import {Product} from './Product'
+export const Data : Product[] =
+[
+  {
+    id:1,
+    name:"item1",
+    price:22.33,
+    desc:"nội dung sản phẩm",
+    status: true
+  },
+  {
+    id:2,
+    name:"item2",
+    price:22.33,
+    desc:"nội dung sản phẩm",
+    status: true
+  },
+  {
+    id:3,
+    name:"item3",
+    price:22.33,
+    desc:"nội dung sản phẩm",
+    status: true
+  }
+]
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Data} from '../MockData';
 
 @Component({
   selector: 'app-product-list',
@@ -11,5 +12,6 @@ export class ProductListComponent implements OnInit {
 
   ngOnInit() {
   }
+  products=Data;
 
 }

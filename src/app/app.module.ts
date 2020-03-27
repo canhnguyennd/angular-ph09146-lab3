9b5011b28f0977040792305b47e5d5f6ae92d185
canhnguyennd <canhnguyennd0308@gmail.com>
2020-03-27 10:42:14 +0700

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ProductComponent } from './product/product.component';
@@ -12,7 +13,7 @@ import { ProductService } from './product.service';
 import { ProductAddComponent } from './product-add/product-add.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, AppRoutingModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent, ProductComponent, ProductListComponent, SlideComponent, CategoryComponent, ProductAddComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ProductService]
